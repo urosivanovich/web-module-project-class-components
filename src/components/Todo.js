2f900b6b250b81ca2e9dbd5db7860ca034d97ca6
Uros Ivanovic <urosivanovic@Uross-MBP.lan>
2022-01-10 18:57:20 -0500

    mvp all classes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Todo.css'
 
-const Todo = props => {
+class Todo extends React.Component {
+    render(){
     return (
-        <div onClick={() => props.toggleTodo(props.item.id)} className={`todo${props.item.completed ? ' completed' : ''}`}>
-        <p>{props.item.task}</p>
+        <div onClick={() => this.props.toggleTodo(this.props.item.id)} className={`todo${this.props.item.completed ? ' completed' : ''}`}>
+        <p>{this.props.item.task}</p>
 
         </div>
 
     )
+    }
 }
 
 export default Todo
